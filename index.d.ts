8a5364f8c30c43939067fc76473a96fcf041be09
Kevin Hu <kevinhuwest@gmail.com>
2022-03-23 19:06:57 -0700

    Add batch option

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,26 @@
 /// <reference types="node" />
 
 declare module 'clickhouse' {
-  import {Stream} from 'stream';
+    import { Stream } from 'stream'
 
-  type callbackExec = (error: Error, rows?: Object[]) => void;
+    type callbackExec = (error: Error, rows?: Object[]) => void
 
-  export class ClickHouse {
-    constructor(opts: Object);
-    query(query: String, reqParams?: object): QueryCursor;
-    insert(query: String, data?: object): QueryCursor;
-    sessionId: string;
-  }
+    export class ClickHouse {
+        constructor(opts: Object)
+        query(query: String, reqParams?: object): QueryCursor
+        insert(query: String, data?: object): QueryCursor
+        sessionId: string
+    }
 
-  export class WriteStream extends Stream.Transform {
-    writeRow(data: Array<any> | string): Promise<void>;
-    exec(): Promise<{}>;
-  }
+    export class WriteStream extends Stream.Transform {
+        writeRows(data: Array<any>): Promise<void>
+        writeRow(data: Array<any> | string): Promise<void>
+        exec(): Promise<{}>
+    }
 
-  class QueryCursor {
-    toPromise(): Promise<Object[]>;
-    exec(callback: callbackExec): void;
-    stream(): Stream & WriteStream;
-  }
+    class QueryCursor {
+        toPromise(): Promise<Object[]>
+        exec(callback: callbackExec): void
+        stream(): Stream & WriteStream
+    }
 }
